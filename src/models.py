0f7e96328a2eb60f7d85784084bcb1df93b72cc4
AbduvorisFirst <abduvoris2bbc@gmail.com>
2026-04-10 15:46:03 +0500

    models boldi
    boldi

--- src/models.py
+++ src/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Calculator(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+
+    expression = models.CharField(max_length=255, verbose_name='ifoda')
+    result = models.CharField(max_length=255, verbose_name='natija')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.user.username}: {self.expression} = {self.result}"
+
+
+
