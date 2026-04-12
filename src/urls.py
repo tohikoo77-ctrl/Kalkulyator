078b0a0adf38fefa30e991e50ca71e1bee4af164
Пользователь <kamamaxmed@gmail.com>
2026-04-12 21:38:50 +0500

    fix: views.py

--- src/urls.py
+++ src/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import calculator_view
+from .views import calculator_view, calculate_api, history_api
 
 urlpatterns = [
-    path('', calculator_view, name='calculator'),
+    path('', calculator_view, name='home'),
+    path('api/calculate/', calculate_api),
+    path('api/history/', history_api),
 ]
--- src/views.py
+++ src/views.py
@@ -1,25 +1,73 @@
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
 from .models import Calculator
+from django.contrib.auth.models import User
+import json
+
+def get_user(request):
+    if request.user.is_authenticated:
+        return request.user
+    return User.objects.first()  
+
 
 def calculator_view(request):
+    user = get_user(request)
+
     if request.method == "POST":
         expression = request.POST.get("expression")
 
         try:
-            result = str(eval(expression))
+            result = str(str + int(expression))
         except:
             result = "Xatolik"
 
         Calculator.objects.create(
-            user=request.user,
+            user=user,
             expression=expression,
             result=result
         )
 
-        return redirect('calculator')
+        return redirect('home')
 
-    calculations = Calculator.objects.filter(user=request.user)
+    calculations = Calculator.objects.filter(user=user)
 
     return render(request, 'dizayn.html', {
         'calculations': calculations
     })
+
+def calculate_api(request):
+    if request.method == "POST":
+        user = get_user(request)
+
+        try:
+            data = json.loads(request.body)
+            expression = data.get("expression")
+
+            result = str(eval(expression))
+
+            calc = Calculator.objects.create(
+                user=user,
+                expression=expression,
+                result=result
+            )
+
+            return JsonResponse({
+                "id": calc.id,
+                "expression": expression,
+                "result": result
+            })
+
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=400)
+
+    return JsonResponse({"error": "Only POST method allowed"}, status=405)
+
+
+def history_api(request):
+    user = get_user(request)
+
+    calculations = Calculator.objects.filter(user=user).values(
+        'id', 'expression', 'result', 'created_at'
+    )
+
+    return JsonResponse(list(calculations), safe=False)
